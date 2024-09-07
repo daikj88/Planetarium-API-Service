82af982afa7debae57a676bed0c13393098740d1
daikj <tyutyunniksolomia@gmail.com>
2024-09-07 11:52:33 +0300

    implement planetraium model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,73 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}, row: {self.rows}, seats: {self.seats_in_row}"
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    show_theme = models.ManyToManyField(
+        "ShowTheme",
+        related_name="show_theme",
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.astronomy_show.title} {self.planetarium_dome.name} {self.show_time}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    class Meta:
+        unique_together = ["seat", "show_session"]
+
+    def __str__(self):
+        return f"{self.row} {self.seat} {self.show_session}"
+
