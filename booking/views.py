c92f9b2211004a90c9201d5f90f5e8de76e2806d
daikj <tyutyunniksolomia@gmail.com>
2024-09-07 14:58:53 +0300

    implement base view

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from booking.models import (
+    AstronomyShow,
+    PlanetariumDome,
+    ShowTheme,
+    ShowSession,
+    Ticket,
+    Reservation
+)
+from booking.serializers import (
+    AstronomyShowSerializer,
+    PlanetariumDomeSerializer,
+    ShowThemeSerializer,
+    TicketSerializer,
+    ReservationSerializer,
+    ShowSessionSerializer
+)
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.all()
+    serializer_class = ShowSessionSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
