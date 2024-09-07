901c62f72090dc3ec9afa5da79494a11ccf48ffd
daikj <tyutyunniksolomia@gmail.com>
2024-09-07 11:55:03 +0300

    add admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from booking.models import (
+    ShowSession,
+    PlanetariumDome,
+    ShowTheme,
+    AstronomyShow,
+    Reservation,
+    Ticket
+)
+
+admin.site.register(ShowSession)
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
+admin.site.register(PlanetariumDome)
