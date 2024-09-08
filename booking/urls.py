b9078a0c63f8fabac06737874878ed6aeac231dd
daikj <tyutyunniksolomia@gmail.com>
2024-09-08 13:54:03 +0300

    add routers to urls

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from booking.views import (
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+    PlanetariumDomeViewSet,
+    ShowSessionViewSet,
+    ReservationViewSet
+)
 
 routers = routers.DefaultRouter()
+routers.register("astronomy_shows", AstronomyShowViewSet)
+routers.register("planetarium_domes", PlanetariumDomeViewSet)
+routers.register("show_themes", ShowThemeViewSet)
+routers.register("show_sessions", ShowSessionViewSet)
+routers.register("reservations", ReservationViewSet)
 
 urlpatterns = [
     path("", include(routers.urls)),
