43b3a9e51405a832843ee7e474e450bfd9d48b29
daikj <tyutyunniksolomia@gmail.com>
2024-09-11 02:57:11 +0300

    add path for user

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("booking.urls", namespace="planetarium")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
