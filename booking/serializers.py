91bb3f164a7d24f09bd4c1e80d109693c7a87e73
daikj <tyutyunniksolomia@gmail.com>
2024-09-10 23:13:20 +0300

    overwrite the create method

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from booking.models import (
@@ -88,9 +89,16 @@ class ShowSessionRetrieveSerializer(ShowSessionSerializer):
 
 
 class ReservationSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
     class Meta:
         model = Reservation
-        fields = ("id", "created_at", "user")
+        fields = ("id", "created_at", "tickets")
 
-
-
+    @transaction.atomic
+    def create(self, validated_data):
+        tickets_data = validated_data.pop("tickets")
+        reservation = Reservation.objects.create(**validated_data)
+        for ticket_data in tickets_data:
+            Ticket.objects.create(reservation=reservation, **ticket_data)
+        return reservation
