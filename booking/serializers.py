25f7daa705c067338a8c2cacb4e469b7a0c0e67b
daikj <tyutyunniksolomia@gmail.com>
2024-09-11 02:24:58 +0300

    add field images

--- booking/serializers.py
+++ booking/serializers.py
@@ -20,7 +20,7 @@ class ShowThemeSerializer(serializers.ModelSerializer):
 class AstronomyShowSerializer(serializers.ModelSerializer):
     class Meta:
         model = AstronomyShow
-        fields = ("id", "title", "description", "show_theme")
+        fields = ("id", "title", "description", "show_theme", "images")
 
 
 class AstronomyShowListSerializer(AstronomyShowSerializer):
