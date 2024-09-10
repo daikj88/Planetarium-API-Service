ad2c9e3fed1125cdf004ce73fdab6a3ed39c962c
daikj <tyutyunniksolomia@gmail.com>
2024-09-11 01:19:31 +0300

    create ManageUserView and add endpoint to urls

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage")
 ]
